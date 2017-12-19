86517dfcb121321ab26c3623ded0e18e4075cab4
ThisIsNSH <hadanis.singh@gmail.com>
2017-12-19 11:28:38 +0530

    enter animation

--- app/src/main/java/com/education/rajneesh/jrec/coursesexpand.java
+++ app/src/main/java/com/education/rajneesh/jrec/coursesexpand.java
@@ -20,9 +20,12 @@ public class coursesexpand extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coursesexpand);
 
-        final TextView intro = (TextView) findViewById(history);
-        final TextView contact1 = (TextView) findViewById(contacts);
-        final TextView gallery1 = (TextView) findViewById(gallery);
+        final TextView intro = (TextView) findViewById(R.id.rscit);
+        final TextView contact1 = (TextView) findViewById(R.id.hard);
+        final TextView gallery1 = (TextView) findViewById(R.id.pmkvy);
+        final TextView tally1 = (TextView) findViewById(R.id.tally);
+        final TextView pwd1 = (TextView) findViewById(R.id.pwd);
+
         int test1[] = new int[2];
         intro.getLocationInWindow(test1);
 
@@ -30,11 +33,20 @@ public class coursesexpand extends AppCompatActivity {
         ValueAnimator a = ObjectAnimator.ofFloat(intro, "x", -10, 60).setDuration(1000);
         ValueAnimator b = ObjectAnimator.ofFloat(contact1, "x", -10, 60).setDuration(1000);
         ValueAnimator c = ObjectAnimator.ofFloat(gallery1, "x", -10, 60).setDuration(1000);
+        ValueAnimator d = ObjectAnimator.ofFloat(tally1, "x", -10, 60).setDuration(1000);
+        ValueAnimator e = ObjectAnimator.ofFloat(pwd1, "x", -10, 60).setDuration(1000);
+
         b.setStartDelay(300);
         c.setStartDelay(500);
+        d.setStartDelay(700);
+        e.setStartDelay(900);
+
         ValueAnimator c1 = ObjectAnimator.ofFloat(gallery1, "alpha", 0, 1).setDuration(1500);
         ValueAnimator a1 = ObjectAnimator.ofFloat(intro, "alpha", 0, 1).setDuration(1500);
         ValueAnimator b1 = ObjectAnimator.ofFloat(contact1, "alpha", 0, 1).setDuration(1500);
+        ValueAnimator e1 = ObjectAnimator.ofFloat(tally1, "alpha", 0, 1).setDuration(1500);
+        ValueAnimator d1 = ObjectAnimator.ofFloat(pwd1, "alpha", 0, 1).setDuration(1500);
+
 
         final AnimatorSet anim = new AnimatorSet();
 
@@ -54,10 +66,23 @@ public class coursesexpand extends AppCompatActivity {
                 gallery1.setVisibility(View.VISIBLE);
             }
         }, 550);
-        
+        tally1.postDelayed(new Runnable() {
+            public void run() {
+                tally1.setVisibility(View.VISIBLE);
+            }
+        }, 750);
+        pwd1.postDelayed(new Runnable() {
+            public void run() {
+                pwd1.setVisibility(View.VISIBLE);
+            }
+        }, 950);
+
         anim.play(a).with(a1);
         anim.play(b).with(b1);
         anim.play(c).with(c1);
+        anim.play(d).with(d1);
+        anim.play(e).with(e1);
+
         anim.start();
 
 
