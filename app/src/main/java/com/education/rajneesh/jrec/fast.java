1da57543f62e2b8a96d89239eea1b8a080039f02
ThisIsNSH <hadanis.singh@gmail.com>
2017-12-24 10:32:25 +0530

    fast.xml added

---
+++ app/src/main/java/com/education/rajneesh/jrec/fast.java
@@ -0,0 +1,13 @@
+package com.education.rajneesh.jrec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class fast extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fast);
+    }
+}
