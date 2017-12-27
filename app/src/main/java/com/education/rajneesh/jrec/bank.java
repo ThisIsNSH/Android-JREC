cc428b1f507321f69a39940e1c1215ac3ed3d217
ThisIsNSH <hadanis.singh@gmail.com>
2017-12-27 19:32:23 +0530

    bank.java

---
+++ app/src/main/java/com/education/rajneesh/jrec/bank.java
@@ -0,0 +1,18 @@
+package com.education.rajneesh.jrec;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by this-is-nsh on 27/12/17.
+ */
+
+public class bank extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutexpand);
+
+    }
+}
